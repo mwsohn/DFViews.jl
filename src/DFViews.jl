0af42131db4a85e9a6e30f8befafbff7d8d7c8fc
Min-Woong Sohn <mwsohn@gmail.com>
2018-09-10 00:21:35 -0400

    Updates for Julia 1.0.0

--- src/DFViews.jl
+++ src/DFViews.jl
@@ -2,7 +2,7 @@ module DFViews
 
 using DataFrames, Electron, Labels
 
-export dfview
+export dfview, dfdesc
 
 function getmaxwidth(vec::AbstractVector)
     return maximum(length.(collect(skipmissing(vec))))
@@ -10,11 +10,11 @@ end
 
 function atype(df::DataFrame,v::Symbol)
     # Array type = DA for DataArray, CA for Categorical Array, and UV for Union Vector
-    if isdefined(:CategoricalArrays) && typeof(df[v]) <: CategoricalArray
-        return string("Categorical (",replace(string(eltype(df[v].refs)),"UInt",""),")")
-    elseif isdefined(:DataArrays) && typeof(df[v]) <: DataArray
+    if isdefined(Main,:CategoricalArrays) && typeof(df[v]) <: CategoricalArray
+        return string("Categorical (",replace(string(eltype(df[v].refs)),"UInt" => ""),")")
+    elseif isdefined(Main,:DataArrays) && typeof(df[v]) <: DataArray
          return "DataArray"
-    elseif isdefined(:PooledArrays) && typeof(df[v]) <: PooledArray
+    elseif isdefined(Main,:PooledArrays) && typeof(df[v]) <: PooledArray
          return "PooledArray"
     elseif isa(eltype(df[v]),Union)
         return "Union Vector" # Union Vector
@@ -34,36 +34,42 @@ function etype(df::DataFrame,v::Symbol)
         eltyp = string(Missings.T(eltype(df[v])))
         if in(eltyp,["String","AbstractString"])
             eltyp = string("Str",getmaxwidth(df[v]))
+        elseif eltyp in ["Dates.Date","Dates.DateTime"]
+            eltyp = replace(eltyp,r"^Dates\." => "")
         end
     end
 
     return eltyp
 end
 
-function dfview(df::DataFrame, label::Union{Label,Void} = nothing; width=800, height=850)
+function dfdesc(df::DataFrame, label::Union{Label,Nothing} = nothing; width=800, height=850)
 
     bdata = IOBuffer()
-
-    pkgdir = replace(Pkg.dir(),"\\","/") * "/DFViews/src"
 
     print(bdata,"""
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <title>DataFrame Table of Contents</title>
-  <link href="$pkgdir/tabulator_simple.min.css" rel="stylesheet">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/tabulator/3.5.3/css/tabulator.min.css" rel="stylesheet">
 </head>
 <body style="margin:0;padding:0;font-family:Arial,Helvetica,Tahoma;">
 <div id="df-table"></div>
 
 <!-- Insert this line above script imports  -->
 <script>if (typeof module === 'object') {window.module = module; module = undefined;}</script>
-<script type="text/javascript" src="$pkgdir/jquery-3.3.1.min.js"></script>
-<script type="text/javascript" src="$pkgdir/jquery-ui.min.js"></script>
-<script type="text/javascript" src="$pkgdir/tabulator.min.js"></script>
+<script
+    src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E="
+    crossorigin="anonymous"></script>
+<script
+    src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
+    integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
+    crossorigin="anonymous"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/tabulator/3.5.3/js/tabulator.min.js"></script>
 <script type="text/javascript">
     var tabledata = [
-    """)
+""")
 
     varnames = names(df)
     nrows = size(df,1)
@@ -72,7 +78,7 @@ function dfview(df::DataFrame, label::Union{Label,Void} = nothing; width=800, he
         varname = string(varnames[i])
         atyp = atype(df,varnames[i])
         etyp = etype(df,varnames[i])
-        pmiss = string(round(100 * nmiss[i]/nrows,1),"%")
+        pmiss = string(round(100 * nmiss[i]/nrows,digits=1),"%")
 
         print(bdata,"\t{id:",i,",varname:\"",varname,"\",atype:\"",atyp,
             "\",etype:\"",etyp,"\",nval:",nrows-nmiss[i],",pmiss:\"",pmiss,"\"")
@@ -80,10 +86,10 @@ function dfview(df::DataFrame, label::Union{Label,Void} = nothing; width=800, he
         # if label dictionary is provided
         if label != nothing
             # label name
-            lblname = label.lblname[varnames[i]]
+            lname = lblname(label,varnames[i]) == nothing ? "" : string(lblname(label,varnames[1]))
             # variable label
             vrlab = varlab(label,varnames[i])
-            print(bdata,",lblname:\"",lblname,"\",varlab:\"",replace(vrlab,"\"","\\\""),"\"")
+            print(bdata,",lblname:\"",lname,"\",varlab:\"",replace(vrlab,"\"" => "\\\""),"\"")
         end
 
         println(bdata, "},")
@@ -91,8 +97,7 @@ function dfview(df::DataFrame, label::Union{Label,Void} = nothing; width=800, he
     println(bdata, "];")
 
     println(bdata,"""
-
-\$\(\"#df-table\"\).tabulator({
+    \$("#df-table").tabulator({
     height:800,
     data:tabledata,
     layout:"fitColumns",
@@ -110,7 +115,102 @@ function dfview(df::DataFrame, label::Union{Label,Void} = nothing; width=800, he
 </script>
 </body>
 </html>
-    """)
+
+""")
+
+    fn = tempdir() * "dfdesc_table.html"
+    if isfile(fn)
+        rm(fn)
+    end
+    println(fn)
+    fout = open(fn,"w")
+    print(fout,String(take!(bdata)))
+    close(fout)
+
+    Window(URI(fn),options=Dict("width"=>width,"height"=>height,"x"=>0,"y"=>0))
+
+end
+
+function dfview(df::DataFrame, label::Union{Label,Nothing} = nothing; from = 1, maxrows = 1000, width=800, height=850)
+
+    bdata = IOBuffer()
+
+    print(bdata,"""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>DataFrame Table of Contents</title>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/tabulator/3.5.3/css/tabulator.min.css" rel="stylesheet">
+</head>
+<body style="margin:0;padding:0;font-family:Arial,Helvetica,Tahoma;">
+<div id="df-table"></div>
+
+<!-- Insert this line above script imports  -->
+<script>if (typeof module === 'object') {window.module = module; module = undefined;}</script>
+<script
+    src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E="
+    crossorigin="anonymous"></script>
+<script
+    src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
+    integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
+    crossorigin="anonymous"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/tabulator/3.5.3/js/tabulator.min.js"></script>
+<script type="text/javascript">
+    var tabledata = [
+""")
+
+    varnames = names(df)
+    nrows,ncols = size(df)
+
+    to = min(nrows,maxrows + from - 1)
+
+    for i=from:to
+
+        print(bdata,"\t{id:",i)
+        for j=1:ncols
+            if occursin("Str",etype(df,varnames[j]))
+                str = ismissing(df[i,varnames[j]]) ? "" : df[i,varnames[j]]
+                print(bdata,",",string("f_",varnames[j]),":\"",str,"\"")
+            elseif etype(df,varnames[j]) in ["Date","DateTime"]
+                str = ismissing(df[i,varnames[j]]) ? "" : string(df[i,varnames[j]])
+                print(bdata,",",string("f_",varnames[j]),":\"",str,"\"")
+            elseif ismissing(df[i,varnames[j]])
+                print(bdata,",",string("f_",varnames[j]),":")
+            else
+                print(bdata,",",string("f_",varnames[j]),":",df[i,varnames[j]])
+            end
+        end
+        println(bdata,"},")
+
+    end
+    println(bdata, "];")
+
+    println(bdata,"""
+    \$("#df-table").tabulator({
+    height:800,
+    data:tabledata,
+    layout:"fitColumns",
+    columns:[""")
+
+    println(bdata,"{title:\"Row\",field:\"id\",align:\"right\"},")
+    for i=1:ncols
+        if occursin("Str",etype(df,varnames[i]))
+            align="left"
+        else
+            align="right"
+        end
+
+        println(bdata,"\t{title:\"",string(varnames[i]),"\",field:\"",string("f_",varnames[i]),"\",align:\"",align,"\"},")
+    end
+    println(bdata,"""
+    ],
+});
+</script>
+</body>
+</html>
+
+""")
 
     fn = tempdir() * "dfview_table.html"
     if isfile(fn)
@@ -124,5 +224,6 @@ function dfview(df::DataFrame, label::Union{Label,Void} = nothing; width=800, he
     Window(URI(fn),options=Dict("width"=>width,"height"=>height,"x"=>0,"y"=>0))
 
 end
+
 
 end # module
